4128b4d9340101d960f9d6a62023645d62723ad4
Prerana Haridoss <preranah@bu.edu>
2016-10-02 20:24:07 -0400

    Added theme and tried adding google sign in.

--- app/src/main/java/com/example/prerana/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/prerana/myfirstapp/DisplayMessageActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import java.util.Random;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -15,9 +16,21 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String name = intent.getStringExtra(MainActivity.NAME);
+
         TextView textView = new TextView(this);
         textView.setTextSize(40);
-        textView.setText(message);
+        //textView.setText(message);
+        if (message.endsWith("?")){
+        Random r = new Random();
+        int i = r.nextInt(100);
+        //textView.setText(message);
+        String m = Integer.toString(i);
+        textView.setText("Hi "+name+",\n"+m+"%  YES. \nAll the best making your decision");}
+        else{
+            textView.setText("Please enter a question with a '?' in the end for a valid answer");
+        }
+
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
--- app/src/main/java/com/example/prerana/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/prerana/myfirstapp/MainActivity.java
@@ -1,28 +1,94 @@
 package com.example.prerana.myfirstapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.auth.api.Auth;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public final static String NAME = "com.example.myfirstapp.NAME";
+
+    private SignInButton btnLogin;
+    private GoogleSignInOptions googleSignInOptions;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int REQUEST_CODE_LOGIN = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /** GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // [END configure_signin]
+
+        // [START build_client]
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        btnLogin = (SignInButton) findViewById(R.id.signin_button);
+        btnLogin.setSize(SignInButton.SIZE_ICON_ONLY);
+        btnLogin.setScopes(googleSignInOptions.getScopeArray());
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent signin = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signin,REQUEST_CODE_LOGIN);
+            }
+
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_LOGIN){
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            GoogleSignInAccount account = googleSignInResult.getSignInAccount();
+            String personName = account.getGivenName();
+        }
+        else{
+            Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+        }**/
     }
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
+        EditText nameText = (EditText) findViewById(R.id.name);
         String message = editText.getText().toString();
+        String name = nameText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(NAME, name);
         startActivity(intent);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
 
