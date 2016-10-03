f5a4fb87e07058865c37b5825d4cf379590a2022
Prerana Haridoss <preranah@bu.edu>
2016-10-02 21:56:33 -0400

    Added comments etc

---
+++ README.md
@@ -0,0 +1,37 @@
+#Decision Maker App
+
+Helps in making a decision. 
+
+##Enter Details
+
+Enter your name and your question.
+
+##Images of the app
+
+1. Home Page
+
+![alt text](https://github.com/preranah/MyFirstApp/blob/master/images/page1APP.png)
+
+2. Home page with details
+
+When a question is asked
+
+![alt text](https://github.com/preranah/MyFirstApp/blob/master/images/page2APP.png)
+
+When a question is submitted
+
+![alt text](https://github.com/preranah/MyFirstApp/blob/master/images/page3APP.png)
+
+When the input is not a question
+
+![alt text](https://github.com/preranah/MyFirstApp/blob/master/images/page4APP.png)
+
+The result is 
+
+![alt text](https://github.com/preranah/MyFirstApp/blob/master/images/page5APP.png)
+
+##Trello for this project
+
+![alt text](https://github.com/preranah/MyFirstApp/blob/master/images/trello.png)
+
+
--- app/src/main/java/com/example/prerana/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/example/prerana/myfirstapp/DisplayMessageActivity.java
@@ -8,29 +8,30 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+//***********************This is an activity tha that is executed when the question is submitted and it Displays the Message ************
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
+        //**********grabs the intent that started the activity. ************
         Intent intent = getIntent();
+        //**********retrieves the data from the first activity.***********
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         String name = intent.getStringExtra(MainActivity.NAME);
 
         TextView textView = new TextView(this);
         textView.setTextSize(40);
-        //textView.setText(message);
+        //******Check if the the entry is a question********
         if (message.endsWith("?")){
         Random r = new Random();
         int i = r.nextInt(100);
-        //textView.setText(message);
         String m = Integer.toString(i);
         textView.setText("Hi "+name+",\n"+m+"%  YES. \nAll the best making your decision");}
         else{
             textView.setText("Please enter a question with a '?' in the end for a valid answer");
         }
-
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
